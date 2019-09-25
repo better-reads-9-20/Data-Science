ad18d917a173f3d8269bf5ce9c87776de3f8317c
HadenMoore <HadenMoore@users.noreply.github.com>
2019-09-25 17:30:00 -0400

    Update

--- API/api.py
+++ API/api.py
@@ -1,12 +1,14 @@
-import flask
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 app.config["DEBUG"] = True
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///better_reads.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+DB = SQLAlchemy(app)
 
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Better Reads Database</h1><p>This site is a prototype API for Better Reads.</p>"
 
 app.run()
-    
