05521727791213e0bc3bc87936eaa7e3e86498f0
noreallyimfine <avrahamjacobsohn@gmail.com>
2019-09-26 13:56:46 -0400

    updated pipfile and app.py

--- test_api/app.py
+++ test_api/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from decouple import config
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///grbetterreads.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 DB = SQLAlchemy(app)
 
