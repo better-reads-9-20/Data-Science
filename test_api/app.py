5c412cfd0f64432a9ee0d22cf8e63a166e35531d
noreallyimfine <avrahamjacobsohn@gmail.com>
2019-09-26 13:33:12 -0400

    put api in one file, added pipfile

--- test_api/app.py
+++ test_api/app.py
@@ -1,14 +1,35 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from model import *
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///betterreads.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///grbetterreads.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 DB = SQLAlchemy(app)
 
+class Book(DB.Model):
+    id = DB.Column(DB.Integer, primary_key=True, autoincrement=True)
+    webpage = DB.Column(DB.BigInteger)
+    title = DB.Column(DB.String(40))
+    author = DB.Column(DB.String(40))
+    descrip = DB.Column(DB.String(2000))
+    rating = DB.Column(DB.Float)
+    num_ratings = DB.Column(DB.String(30))
+    num_reviews = DB.Column(DB.String(30))
+    isbn = DB.Column(DB.String(15))
+    isbn13 = DB.Column(DB.String(15))
+    binding = DB.Column(DB.String(15))
+    edition = DB.Column(DB.String(50))
+    num_pages = DB.Column(DB.String(15))
+    published_on = DB.Column(DB.String(150))
+    genres = DB.Column(DB.String(1000))
+
+    def __repr__(self):
+        return f'Book: {self.title} writtien by {self.author}'
+
+
 @app.route('/api/description', methods=['GET', 'POST'])
-def home():
+def api():
     if request.method == 'POST':
         title = request.form.get('title')
         book = DB.session.query(Book.author,
