fb70c858726944311471f0ec7bf3d3270b3b8412
noreallyimfine <avrahamjacobsohn@gmail.com>
2019-09-26 20:36:18 -0400

    added scipy to requirements

--- app.py
+++ app.py
@@ -45,8 +45,7 @@ def api():
         #print(pred_array)
         output = []
         for pred in pred_array[1][0]:
-            book = DB.session.query(Book.title, Book.author, Book.rating, Book.isbn, 
-                                    Book.isbn13).filter(Book.id=int(pred)).all()[0]
+            book = DB.session.query(Book.title, Book.author, Book.rating, Book.isbn).filter(Book.id=int(pred)).all()[0]
             output.append(book)
         return str(output)
 
